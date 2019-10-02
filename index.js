52d4029fcff7567bdd7a82521a68dab3fec96eb3
Thando <nodwengut@gmail.com>
2019-10-02 09:35:13 +0200

    initial commit

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const exphbs = require('express-handlebars');
 const bodyParser = require('body-parser')
 const flash = require('express-flash');
 const session = require('express-session');
+const router = require('./routes/registrations.js');
 
 const app = express();
 
@@ -20,6 +21,7 @@ app.use(flash());
 app.engine('handlebars', exphbs({ defaultLayout: 'main' }));
 app.set('view engine', 'handlebars');
 
+
 // parse application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: false }))
 // parse application/json
@@ -29,123 +31,17 @@ app.use(express.static('public'));
 
 //Define error-handling middleware functions
 app.use(function (err, req, res, next) {
-   res.status(500);
-   res.render('error', { error: err });
-})
+    res.status(500);
+    res.render('error', { error: "err: testing the error" });
+ })
+
+app.use('/', router);
+app.use('/reg_numbers/', router);
+app.use('/reg_number/:reg_number/', router);
+app.use('/reg_numbers/delete/:reg_number/', router);
 
 
-const { Pool, Client } = require('pg');
-
-let useSSL = false;
-let local = process.env.LOCAL || false;
-if (process.env.DATABASE_URL && !local){
-  useSSL = true;
-}
-
-const connectionString = process.env.DATABASE_URL || 'postgresql://coder:pg123@localhost:5432/registrations_db';
-
-const pool = new Pool({
-  connectionString,
-  ssl : useSSL
-});
-
-const RegistrationNumber = require('./registrationNumber');
-const registrationNumber = RegistrationNumber(pool);
-
-app.get('/', async (req, res, next) => {
-  
-  try {
-    res.render('index', {
-      towns: await registrationNumber.getAllTowns(),
-      regNumbersFromTown: await registrationNumber.getAllFromTown(),
-      messages: req.flash('info')
-    });
-  } catch(err) {
-    next(err);
-  }
-})
-
-app.post('/reg_number/', async (req, res, next) => {
-  try {
-    let reg_number = (req.body.registration).toUpperCase();
-    let isRepeated = await registrationNumber.regIsRepeated(reg_number);
-    let validNumber = await registrationNumber.validRegNumber(reg_number);
-    let letterNumber = /^[0-9a-zA-Z ]+$/;
-
-    if( (!reg_number.match(letterNumber)) || reg_number === "" ) {return}
-
-    if (validNumber === false) {
-      req.flash('info', 'Please add a valid registration number'); 
-    } else {
-      if (!isRepeated) {
-        let registration = {
-          reg_number: reg_number, 
-          town_id: await registrationNumber.getTownId(reg_number)
-        }
-        await registrationNumber.setRegistration(registration)
-        req.flash('info', 'New Record Added');
-      } else {
-        req.flash('info', 'Record Already Exists');
-      }
-    }
-    res.redirect('/')
-  }
-  catch(error) {
-    next(error)
-  }
-});
-
-app.get('/reg_number/:reg_number/', async (req, res, next) => {
-  try {
-    res.render('reg_number', {
-      reg_number: req.params.reg_number
-    });
-  } catch(error) {
-    next(error)
-  }
-});
-
-app.post('/reg_numbers/', async (req, res, next) => {
-  try {
-    let radioValue = req.body.townRadio;
-    if(radioValue) {
-      await registrationNumber.setAllFromTown(radioValue);
-    } else {
-      req.flash('info', 'Please select a town');
-    }
-    res.redirect('/')
-
-  } catch(error) {
-    next(error)
-  }
-})
-
-app.get('/reg_numbers/', async (req, res, next) => {
-  try {
-    res.render('reg_numbers', {
-      registrations: await registrationNumber.getAllRegistrations(),
-      messages: req.flash('info')
-    });
-  } 
-  catch(error) {
-    next(error)
-  }
-})
-
-app.get('/reg_numbers/delete/:reg_number/', async (req, res, next) => {
-  try {
-    let reg_number = req.params.reg_number;
-    await registrationNumber.deleteById(reg_number);
-    req.flash('info', 'Registration number deleted!')
-     
-    res.redirect('/reg_numbers');
-  } catch(error) {
-     next(error)
-  }
-});
-
-
-const PORT = process.env.PORT || 4000;
+const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
     console.log(`App started at http://localhost:${PORT}`);
---
+++ routes/registrations.js
@@ -0,0 +1,115 @@
+const express = require('express');
+const router = express.Router();
+
+const { Pool, Client } = require('pg');
+
+let useSSL = false;
+let local = process.env.LOCAL || false;
+if (process.env.DATABASE_URL && !local){
+  useSSL = true;
+}
+
+const connectionString = process.env.DATABASE_URL || 'postgresql://coder:pg123@localhost:5432/registrations_db';
+
+const pool = new Pool({
+  connectionString,
+  ssl : useSSL
+});
+
+const RegistrationNumber = require('../registrationNumber');
+const registrationNumber = RegistrationNumber(pool);
+
+router.get('/', async (req, res, next) => {
+  
+  try {
+    res.render('index', {
+      towns: await registrationNumber.getAllTowns(),
+      regNumbersFromTown: await registrationNumber.getAllFromTown(),
+      messages: req.flash('info')
+    });
+  } catch(err) {
+    next(err);
+  }
+})
+
+router.post('/reg_number/', async (req, res, next) => {
+  try {
+    let reg_number = (req.body.registration).toUpperCase();
+    let isRepeated = await registrationNumber.regIsRepeated(reg_number);
+    let validNumber = await registrationNumber.validRegNumber(reg_number);
+    let letterNumber = /^[0-9a-zA-Z ]+$/;
+
+    if( (!reg_number.match(letterNumber)) || reg_number === "" ) {return}
+
+    if (validNumber === false) {
+      req.flash('info', 'Please add a valid registration number'); 
+    } else {
+      if (!isRepeated) {
+        let registration = {
+          reg_number: reg_number, 
+          town_id: await registrationNumber.getTownId(reg_number)
+        }
+        await registrationNumber.setRegistration(registration)
+        req.flash('info', 'New Record Added');
+      } else {
+        req.flash('info', 'Record Already Exists');
+      }
+    }
+    res.redirect('/')
+  }
+  catch(error) {
+    next(error)
+  }
+});
+
+router.get('/reg_number/:reg_number/', async (req, res, next) => {
+  try {
+    res.render('reg_number', {
+      reg_number: req.params.reg_number
+    });
+  } catch(error) {
+    next(error)
+  }
+});
+
+router.post('/reg_numbers/', async (req, res, next) => {
+  try {
+    let radioValue = req.body.townRadio;
+    if(radioValue) {
+      await registrationNumber.setAllFromTown(radioValue);
+    } else {
+      req.flash('info', 'Please select a town');
+    }
+    res.redirect('/')
+
+  } catch(error) {
+    next(error)
+  }
+})
+
+router.get('/reg_numbers/', async (req, res, next) => {
+  try {
+    res.render('reg_numbers', {
+      registrations: await registrationNumber.getAllRegistrations(),
+      messages: req.flash('info')
+    });
+  } 
+  catch(error) {
+    next(error)
+  }
+})
+
+router.get('/reg_numbers/delete/:reg_number/', async (req, res, next) => {
+  try {
+    let reg_number = req.params.reg_number;
+    await registrationNumber.deleteById(reg_number);
+    req.flash('info', 'Registration number deleted!')
+     
+    res.redirect('/reg_numbers');
+  } catch(error) {
+     next(error)
+  }
+});
+
+
+module.exports = router;
